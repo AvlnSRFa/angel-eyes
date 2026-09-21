c401216bf30a37cd8515bd6d15f4bed98f9de7ae
AvlnSRFa <Lindee.burton@outlook.com>
2026-09-21 18:02:00 -0400

    Add TOD RVOL 2-minute study

---
+++ TOD_RVol_2m_Large.ts
@@ -0,0 +1,47 @@
+declare upper;
+
+input comparisonDays = 10;
+
+def isTwoMinuteChart =
+    GetAggregationPeriod() == AggregationPeriod.TWO_MIN;
+
+def regularSession =
+    SecondsFromTime(0930) >= 0 and
+    SecondsTillTime(1600) > 0;
+
+def barsPerRegularSession = 195;
+
+def sameTimeVolumeTotal =
+    fold dayNumber = 1 to comparisonDays + 1
+    with total = 0
+    do total + GetValue(
+        volume,
+        dayNumber * barsPerRegularSession,
+        comparisonDays * barsPerRegularSession
+    );
+
+def averageSameTimeVolume =
+    sameTimeVolumeTotal / comparisonDays;
+
+def todRVol =
+    if regularSession and averageSameTimeVolume > 0
+    then volume / averageSameTimeVolume
+    else Double.NaN;
+
+AddLabel(
+    yes,
+    if !isTwoMinuteChart
+    then "TOD RVOL: USE 2-MIN CHART"
+    else if !regularSession
+    then "TOD RVOL: OUTSIDE RTH"
+    else "TOD RVOL  " + AsText(Round(todRVol, 2)) + "x",
+    if !isTwoMinuteChart then Color.RED
+    else if !regularSession then Color.DARK_GRAY
+    else if todRVol >= 3 then Color.MAGENTA
+    else if todRVol >= 2 then Color.CYAN
+    else if todRVol >= 1 then Color.GREEN
+    else Color.GRAY,
+    Location.TOP_LEFT,
+    FontSize.X_LARGE,
+    yes
+);
